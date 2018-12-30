4d8b09d06170220324722358e43cfd7c02b6a27c
Stefano Corra <stefano.corra@hotmail.com>
2018-12-29 22:46:50 -0500

    card player part 2

---
+++ src/components/home/meetPlayers/Cards.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { easePolyOut} from 'd3-ease';
+import Animate from 'react-move/Animate';
+import Otamendi from '../../../Resources/images/players/Otamendi.png'
+
+class HomeCards extends Component {
+
+  state={
+    cards:[{
+              bottom:90,
+              left:300,
+            },
+            {
+              bottom:60,
+              left:200,
+            },
+            {
+              bottom:30,
+              left:100,
+            },
+            {
+              bottom:0,
+              left:0,
+            }]
+  }
+
+  showAnimateCards = () =>(
+      this.state.cards.map((card,i)=>(
+        <Animate
+          show={this.props.show}
+          key={i}
+          start={{
+            left:0,
+            bottom:0
+          }}
+          enter={{
+            left:[card.left],
+            bottom:[card.bottom],
+            timing:{duration:500, easePolyOut}
+          }}
+          >
+          {({left, bottom})=>{
+            return(
+              <div
+                style={{
+                  position: 'absolute',
+                  left,
+                  bottom
+                }}
+              >
+                  div
+              </div>
+            )
+          }}
+        </Animate>
+      ))
+  )
+
+  render() {
+    return (
+      <div>
+        {this.showAnimateCards()}
+      </div>
+    );
+  }
+
+}
+
+export default HomeCards;
--- src/components/home/meetPlayers/index.js
+++ src/components/home/meetPlayers/index.js
@@ -1,66 +1,75 @@
 import React, { Component } from 'react';
 import Stripes from '../../../Resources/images/stripes.png';
 import { Tag } from '../../ui/misc';
+import Reveal from 'react-reveal';
+import Homecards from './Cards';
 
 class MeetPlayers extends Component {
 
   state={
-
+    show:false
   }
 
   render() {
     return (
-      <div className="home_meetplayers"
-          style={{background:`#fff url(${Stripes})`}}>
-          <div className="container">
-            <div className="home_meetplayers_wrapper">
-              <div className="home_card_wrapper">
-                card
-              </div>
-              <div className="home_text_wrapper">
-                <div>
-                  <Tag
-                    bck="#0e1731"
-                    size="100px"
-                    color="#fff"
-                    marginBottom ='20px'>
-                    Meet
-                  </Tag>
+      <Reveal
+        fraction={0.7}
+        onReveal={()=>{
+          this.setState({show:true})
+        }}>
+        <div className="home_meetplayers"
+            style={{background:`#fff url(${Stripes})`}}>
+            <div className="container">
+              <div className="home_meetplayers_wrapper">
+                <div className="home_card_wrapper">
+                  <Homecards  show={this.state.show}/>
                 </div>
-                <div>
-                  <Tag
-                    bck="#0e1731"
-                    size="100px"
-                    color="#fff"
-                    marginBottom ='20px'>
-                    The
-                  </Tag>
-                </div>
-                <div>
-                  <Tag
-                    bck="#0e1731"
-                    size="100px"
-                    color="#fff"
-                    marginBottom ='20px'>
-                    Players
-                  </Tag>
-                </div>
-                <div>
-                  <Tag
-                    bck="#fff"
-                    size="27px"
-                    color="#0e1731"
-                    link={true}
-                    linkto="/the_team"
-                    marginBottom="27px"
-                    border="1px solid #0e1731">
-                    Meet them here
-                  </Tag>
+                <div className="home_text_wrapper">
+                  <div>
+                    <Tag
+                      bck="#0e1731"
+                      size="100px"
+                      color="#fff"
+                      marginBottom ='20px'>
+                      Meet
+                    </Tag>
+                  </div>
+                  <div>
+                    <Tag
+                      bck="#0e1731"
+                      size="100px"
+                      color="#fff"
+                      marginBottom ='20px'>
+                      The
+                    </Tag>
+                  </div>
+                  <div>
+                    <Tag
+                      bck="#0e1731"
+                      size="100px"
+                      color="#fff"
+                      marginBottom ='20px'>
+                      Players
+                    </Tag>
+                  </div>
+                  <div>
+                    <Tag
+                      bck="#fff"
+                      size="27px"
+                      color="#0e1731"
+                      link={true}
+                      linkto="/the_team"
+                      marginBottom="27px"
+                      border="1px solid #0e1731">
+                      Meet them here
+                    </Tag>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-      </div>
+        </div>
+      </Reveal>
+
     );
   }
 
