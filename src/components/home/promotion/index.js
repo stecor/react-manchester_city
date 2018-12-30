37b5a272af53bf92fca05209a7c55a59fbacf4b0
Stefano Corra <stefano.corra@hotmail.com>
2018-12-29 23:23:08 -0500

    promotion

---
+++ src/components/home/promotion/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PromotionAnimation from './Animation';
+
+const Promotion = () => (
+    <div
+      className="promotion_wrapper"
+      style={{
+        background:"#fff"
+      }}>
+        <div className="container">
+          <PromotionAnimation />
+        </div>
+    </div>
+);
+
+export default Promotion;
