e05730fded4c45f00bd6f08b9409ca0d2c25eef7
Stefano Corra <stefano.corra@hotmail.com>
2019-01-01 22:37:47 -0500

    dashboard login/logout

--- src/routes.js
+++ src/routes.js
@@ -4,17 +4,16 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/home';
 import SignIn from './components/signin';
 import Dashboard from './components/admin/Dashboard'
+import PrivateRoutes from './components/authRoutes/privateRoutes';
 
 const Routes = (props) =>{
-
-  console.log(props);
 
   return(
     <Layout>
       <Switch>
+        <PrivateRoutes {...props} exact path="/dashboard" component={Dashboard} />
         <Route  exact path="/" component={Home} />
         <Route  exact path="/sign_in" component={SignIn} />
-        <Route  exact path="/dashboard" component={Dashboard} />
       </Switch>
     </Layout>
   )
